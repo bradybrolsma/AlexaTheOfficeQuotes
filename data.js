fe5fe8fad0db67ecdcb11e061b93de0f0f766549
dlowrey <dane.r.lowrey@gmail.com>
2017-07-05 18:27:41 -0500

    Initial commit of all current project files

---
+++ data.js
@@ -0,0 +1,49 @@
+module.exports = {
+  'Dwight Schrute': [
+   "There's too many people on this Earth. We need a new plague.",
+   "Ryan the fire guy... Ryan started the fire",
+   "When I die, I wanna be frozen. And if they have to freeze me in pieces, so be it. I will wake up stronger than ever, because I will have used that time to figure out exactly why I died, and what moves I could have used to defend myself better - now that I know what hold he had me in.",
+   "I really should have a tweeter account.",
+   "Identity theft is not a joke Jim! Millions of families suffer every year!",
+   "Blink once if you want me to pull the plug."
+  ],
+  'Michael Scott': [
+    "Are you kidding me? I'd go anywhere to see a turtle!",
+    "I need a username. And... I have a great one. Little kid lover. That way, people will know exactly where my priorities are at.",
+    "I once went 28 years without having sex. And then again for seven years.",
+    "I... DECLARE... BANKRUPTCY!",
+    "We'll auction off people, like in the olden days.",
+    "Dwight you IGNORANT SLUT!",
+    "I'm not superstitous, but... I am a little stitous.",
+    "If I had a gun with two bullets and I was in a room with Hitler, Bin Laden, and Toby, I would shoot Toby twice.",
+    "Holly, You and I are soup snakes.",
+    "Toby works in HR, which is technically means he works for corporate, so he's really not part of our family. Also, he's divorced, so he's really not a part of his family.",
+    "You don't know me... you've just seen my penis.",
+    "Well, happy birthday, Jesus. Sorry your party's so lame.",
+    "Bros before hoes. Why? Because your bros are always there for you. They have got your back, after your hoe rips your heart out for no good reason. And you were nothing but great to your hoe. And you told her she was the only hoe for you. And that she was better than all the other hoes in the world. And then… suddenly she’s not your hoe no mo’."
+  ],
+  'Pam Beasley': [
+    "It's performance review day, company-wide. Last year, my performance review started with Michael asking me what my hopes and dreams were, and it ended with him telling me he could bench-press 190 pounds. So, I don't really know what to expect.",
+
+  ],
+  'Jim Halpart': [
+    "When Michael told us that Jan was pregnant, he led us to believe that he was the father. By telling us that he was the father.",
+    "Bears... Beets... Battlestar Galactica.",
+  ],
+  'Angela Martin': [
+    "Poop is raining from the ceilings. Poop!",
+    "It's not a surprise to me. Pam is the office mattress."
+  ],
+  'Kevin Malone': [
+    "I love fake boobs. Often times, you find them on strippers."
+  ],
+  'Creed Bratton': [
+    "I find it offensive. All natural baby. That's how I like 'em. Swing low, sweet chariots.",
+    "Just pretend like we're talking until the cops leave.",
+    "You're paying way too much for worms man. Who's your worm guy?",
+
+  ],
+  'Andy Bernard': [
+    "Jim Halpert's off the market. Guess who just became the best looking single guy in the office?"
+  ]
+}
---
+++ index.js
@@ -0,0 +1,119 @@
+const Alexa = require('alexa-sdk');
+const data = require('./data.js');
+
+const APP_ID = "amzn1.ask.skill.efda1099-9dcc-4576-bfe1-15421985bd00";
+const SKILL_NAME = "The Office Quotes & Quiz";
+const HELP_MESSAGE = "You can say give me a quote, or, you can say quiz me.. What can I help you with?";
+const HELP_REPROMPT = "What can I help you with?";
+const STOP_MESSAGE = "Goodbye!";
+
+function checkAnswer() {
+  // Get the character that the user guessed from the Character slot
+  const current = this.event.request.intent.slots.Character.value;
+  // Get who said the quote (we stored in in the session variables)
+  const previous = this.attributes.Character;
+
+  let speechOutput = '';
+  if (!previous) { // If there was no character stored in session
+    speechOutput = "Oh, I didn't know we were playing. Would you like to play The Office Quiz?"
+  } else if (previous.toLowerCase().includes(current.toLowerCase())) { // if they match
+    speechOutput = "Correct! Would you like to try another?";
+  } else { // if they dont match
+    speechOutput = "I'm sorry, that quote was from " + previous + ". Would you like to try again?";
+  }
+  return speechOutput;
+}
+
+/**
+* pick a random character from data.js
+**/
+function pickRandomCharacter(obj) {
+    let result;
+    let count = 0;
+    for (let prop in obj) // for each property in obj
+        if (Math.random() < 1/++count)
+           result = prop;
+    return result;
+}
+
+/**
+* pick a random quote from a specified character in data.js
+**/
+function pickRandomQuote(character) {
+  let index = Math.floor(Math.random() * data[character].length);
+  let quotes = data[character];
+  let randomQuote = quotes[index];
+  return randomQuote;
+}
+
+/**
+* get a new random quote with who said it
+**/
+function getNewQuote() {
+  const character = pickRandomCharacter(data);
+  const quote = pickRandomQuote(character);
+  const speechOutput = quote + ' ' + character;
+  return speechOutput;
+}
+
+/**
+* get a new random quote without who said it for the quiz game
+**/
+function quizGameQuote() {
+  const character = pickRandomCharacter(data);
+  const quote = pickRandomQuote(character);
+  this.attributes.Character = character; // store character in session
+  return quote;
+}
+
+
+const handlers = {
+    // LaunchRequest when user says 'Alexa, open office quotes'
+    'LaunchRequest': function () {
+        this.emit('GetNewQuoteIntent');
+    },
+    // GetNewQuoteIntent when user says 'Give me a quote'
+    'GetNewQuoteIntent': function () {
+        const speechOutput = getNewQuote.call(this);
+        this.emit(':tellWithCard', speechOutput, SKILL_NAME, speechOutput);
+    },
+    // PlayQuoteQuizIntent when user says 'quiz me'
+    'PlayQuoteQuizIntent': function () {
+      this.emit(':ask', 'I will say a quote by one of the characters from The Office, and you will try to guess who said it. Ready?', 'Say yes when you are ready.')
+    },
+    // GuessIntent when the user guesses with a name i.e. 'dwight'
+    'GuessIntent': function () {
+      const speechOutput = checkAnswer.call(this);
+      this.emit(':ask', speechOutput, 'Would you like to play again?');
+    },
+    // YesIntent for when the user answers in the affirmative
+    'YesIntent': function () {
+      const quote = quizGameQuote.call(this);
+      const question = "Which character from The Office said the quote: "
+      const speechOutput = question + quote;
+      this.emit(':tellWithCard', speechOutput, SKILL_NAME, speechOutput);
+    },
+    // HelpIntent when user says 'Help'
+    'AMAZON.HelpIntent': function () {
+        const speechOutput = HELP_MESSAGE;
+        const reprompt = HELP_REPROMPT;
+        this.emit(':ask', speechOutput, reprompt);
+    },
+    // CancelIntent when the user says 'cancel'
+    'AMAZON.CancelIntent': function () {
+        this.emit(':tell', STOP_MESSAGE);
+    },
+    // StopIntent when the user says 'no' or 'stop'
+    'AMAZON.StopIntent': function () {
+        this.emit(':tell', STOP_MESSAGE);
+    }
+};
+
+// Give the handler object and it's functions to the lambda for
+// execution
+exports.handler = function(event, context, callback) {
+    const alexa = Alexa.handler(event, context); // create alexa obj
+    alexa.appId = APP_ID; // register app id
+    alexa.registerHandlers(handlers); // register handlers
+    alexa.execute(); // begin
+};
